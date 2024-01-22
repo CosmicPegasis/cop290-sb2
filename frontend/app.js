878cd4c8e4d715fad64fa9705a81904c0f2bb2ee
Aviral Singh <aviralsingh@protonmail.com>
2024-01-22 11:38:11 +0530

    Add basic request interface

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,43 @@
-console.log("helo wolrd");
+const backend = "http://localhost:5000";
+
+async function get_symbol_data() {
+  try {
+    symbol = await fetch(`${backend}/request/get_symbols/`, {
+      mode: "cors",
+      credentials: "same-origin",
+    });
+    res = await symbol.json();
+    return res;
+  } catch (err) {}
+}
+
+async function create_list() {
+  data = await get_symbol_data();
+  html = "";
+  for (let symbol in data.AVG_PRICE) {
+    html += `${symbol}<br>`;
+  }
+  return html;
+}
+
+async function get_past_years(symbol, years) {
+  try {
+    symbol = await fetch(`${backend}/request/query/${symbol}/${years}`, {
+      mode: "cors",
+      credentials: "same-origin",
+    });
+    res = await symbol.json();
+    return res;
+  } catch (err) {}
+}
+let symbolForm = document.getElementById("symbol-form");
+symbolForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  let symbol = document.getElementById("symbol-input").value;
+  let years = document.getElementById("year-input").value;
+  console.log("hello!");
+  if (symbol != "" && years != "") {
+    let data = await get_past_years(symbol, years);
+    console.log(data);
+  }
+});
