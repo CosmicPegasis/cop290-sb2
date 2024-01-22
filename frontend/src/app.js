3666584028d29020eca8c5e597e1b3b337135d44
Aviral Singh <aviralsingh@protonmail.com>
2024-01-22 15:37:09 +0530

    Add minimal support for multiple axes

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -30,49 +30,134 @@ async function get_past_years(symbol, years) {
     return res;
   } catch (err) {}
 }
-let symbolForm = document.getElementById("symbol-form");
 let chart = null;
-symbolForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  let symbol = document.getElementById("symbol-input").value;
-  let years = document.getElementById("year-input").value;
-  if (symbol != "" && years != "") {
-    let data = await get_past_years(symbol, years);
+let cur_symbols = ["SBIN", "PAYTM"];
+let years = 10;
+let cur_selections = [];
+let symbolForm = document.getElementById("symbol-form");
+
+// Create add and subtract buttons (later)
+async function redraw() {
+  let graph_metrics = [];
+  let graph_dates = [];
+  for (let i = 0; i < cur_symbols.length; i++) {
+    const symbol = cur_symbols[i];
+    const data = await get_past_years(symbol, years);
+    console.log("reached here");
+    const selections = data.CLOSE; // add map for different selections
+    metrics = [];
     dates = [];
-    close_prices = [];
     let dateObj = data.DATE;
     for (let idx in dateObj) {
       date = dateObj[idx].replace("00:00:00 GMT", "");
       dates.push(date);
     }
-    for (let close_price in data.CLOSE) {
-      close_prices.push(data.CLOSE[close_price]);
+    for (let sel in selections) {
+      metrics.push(selections[sel]);
     }
-    close_prices.reverse();
+    metrics.reverse();
     dates.reverse();
-    const ctx = document.getElementById("myChart");
-    if (chart) {
-      chart.destroy();
+    if (dates.length > graph_dates.length) {
+      console.log(graph_dates);
+      graph_dates = dates;
     }
-    chart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: dates,
-        datasets: [
-          {
-            label: symbol,
-            data: close_prices,
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
+    metric_dataset = { label: symbol, data: metrics };
+    graph_metrics.push(metric_dataset);
+  }
+  const ctx = document.getElementById("myChart");
+  if (chart) {
+    chart.destroy();
+  }
+  let max_metric_size = 0;
+  console.log(dates.length);
+  for (let i = 0; i < graph_metrics.length; i++) {
+    metric_size = graph_metrics[i].data.length;
+    max_metric_size =
+      metric_size > max_metric_size ? metric_size : max_metric_size;
+  }
+  for (let i = 0; i < graph_metrics.length; i++) {
+    metric_size = graph_metrics[i].data.length;
+    if (metric_size < max_metric_size) {
+      let padding = Array(max_metric_size - metric_size).fill(0);
+      padding = padding.concat(graph_metrics[i].data);
+      graph_metrics[i].data = padding;
+    }
+  }
+  chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: graph_dates,
+      datasets: graph_metrics,
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          grid: { display: false },
+        },
+        x: {
+          grid: { display: false },
         },
       },
-    });
-  }
-});
+      responsive: true,
+      elements: {
+        point: {
+          pointStyle: false,
+        },
+      },
+    },
+  });
+}
+const autocolors = window["chartjs-plugin-autocolors"];
+Chart.register(autocolors);
+redraw();
+// Use in-built colors
+// symbolForm.addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   let symbol = document.getElementById("symbol-input").value;
+//   let years = document.getElementById("year-input").value;
+//   if (symbol != "" && years != "") {
+//     let data = await get_past_years(symbol, years);
+//     dates = [];
+//     close_prices = [];
+//     let dateObj = data.DATE;
+//     for (let idx in dateObj) {
+//       date = dateObj[idx].replace("00:00:00 GMT", "");
+//       dates.push(date);
+//     }
+//     for (let close_price in data.CLOSE) {
+//       close_prices.push(data.CLOSE[close_price]);
+//     }
+//     close_prices.reverse();
+//     dates.reverse();
+//     const ctx = document.getElementById("myChart");
+//     if (chart) {
+//       chart.destroy();
+//     }
+//     chart = new Chart(ctx, {
+//       type: "line",
+//       data: {
+//         labels: dates,
+//         datasets: [
+//           {
+//             label: symbol,
+//             data: close_prices,
+//             borderWidth: 1,
+//           },
+//         ],
+//       },
+//       options: {
+//         scales: {
+//           y: {
+//             beginAtZero: true,
+//             grid: { display: false },
+//           },
+//           x: {
+//             grid: { display: false },
+//           },
+//         },
+//         responsive: true,
+//       },
+//     });
+//   }
+// });
