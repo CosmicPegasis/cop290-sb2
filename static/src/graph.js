9de3a746decaf7086ae4c36b56d33f144aadcb2a
Aviral Singh <aviralsingh@protonmail.com>
2024-01-24 15:52:32 +0530

    Refactor js

--- static/src/graph.js
+++ static/src/graph.js
@@ -54,39 +54,7 @@ function get_time_duration() {
   return document.querySelector("input[name=time]:checked").value;
 }
 // Create add and subtract buttons (later)
-async function redraw() {
-  let graph_metrics = [];
-  let graph_dates = [];
-
-  //  let time_checked = document.querySelector("input[name=time]:checked");
-  for (let i = 0; i < cur_symbols.length; i++) {
-    const symbol = cur_symbols[i];
-    if (symbol == "" || !offset) continue;
-    let time_duration = get_time_duration();
-    const data = await get_past(symbol, offset, time_duration);
-    let selections = get_selections(data);
-    let metrics = [];
-    let dates = [];
-    let dateObj = data.DATE;
-    for (let idx in dateObj) {
-      let date = dateObj[idx].replace("00:00:00 GMT", "");
-      dates.push(date);
-    }
-    for (let sel in selections) {
-      metrics.push(selections[sel]);
-    }
-    metrics.reverse();
-    dates.reverse();
-    if (dates.length > graph_dates.length) {
-      graph_dates = dates;
-    }
-    let metric_dataset = { label: symbol, data: metrics };
-    graph_metrics.push(metric_dataset);
-  }
-  const ctx = document.getElementById("myChart");
-  if (chart) {
-    chart.destroy();
-  }
+function plot_chart(graph_dates, graph_metrics) {
   let max_metric_size = 0;
   for (let i = 0; i < graph_metrics.length; i++) {
     let metric_size = graph_metrics[i].data.length;
@@ -100,6 +68,10 @@ async function redraw() {
       padding = padding.concat(graph_metrics[i].data);
       graph_metrics[i].data = padding;
     }
+  }
+  const ctx = document.getElementById("myChart");
+  if (chart) {
+    chart.destroy();
   }
   chart = new Chart(ctx, {
     type: "line",
@@ -126,13 +98,71 @@ async function redraw() {
     },
   });
 }
+function beautify_dates(dateObj) {
+  let dates = [];
+  for (let idx in dateObj) {
+    let date = dateObj[idx].replace("00:00:00 GMT", "");
+    dates.push(date);
+  }
+  return dates;
+}
+function redraw_stock_details(symbol, metrics) {
+  let stockDetailDiv = document.getElementById("stock-details");
+  let formDiv = document.getElementById("form-div");
+  let compareDiv = document.getElementById("compare-div");
+  stockDetailDiv.classList.add("col-4", "text-center");
+  formDiv.classList.remove("col-6");
+  formDiv.classList.add("col-4");
+  compareDiv.classList.remove("col-6");
+  compareDiv.classList.add("col-4");
+  let delta = metrics[metrics.length - 1] - metrics[0];
+  stockDetailDiv.innerHTML = `<h2 style="color: black; font-weight:bold">${symbol}`;
+  if (delta >= 0) {
+    //fontawesome.com/icons/triangle?f=classic&s=solid
+    stockDetailDiv.innerHTML += `<div class="bg-success-subtle rounded-pill"><h3 class="text-success-emphasis"><i class="fa-solid fa-arrow-trend-up"></i>    ${delta.toFixed(
+      2
+    )}</h3></div>`;
+  } else {
+    stockDetailDiv.innerHTML += `<div class="bg-danger-subtle rounded-pill"><h3 class="text-danger-emphasis"><i class="fa-solid fa-arrow-trend-down"></i>   ${delta.toFixed(
+      2
+    )}</h3></div>`;
+  }
+}
+async function redraw() {
+  let graph_metrics = [];
+  let graph_dates = [];
+  for (let i = 0; i < cur_symbols.length; i++) {
+    const symbol = cur_symbols[i];
+    if (symbol == "" || !offset) continue;
+    let time_duration = get_time_duration();
+    const data = await get_past(symbol, offset, time_duration);
+    let selections = get_selections(data);
+    let metrics = [];
+    let dateObj = data.DATE;
+    let dates = beautify_dates(dateObj);
+    for (let sel in selections) {
+      metrics.push(selections[sel]);
+    }
+    metrics.reverse();
+    dates.reverse();
+    if (dates.length > graph_dates.length) {
+      graph_dates = dates;
+    }
+    let metric_dataset = { label: symbol, data: metrics };
+    graph_metrics.push(metric_dataset);
+    if (i == 0) {
+      redraw_stock_details(symbol, metrics);
+    }
+  }
+  plot_chart(graph_dates, graph_metrics);
+}
 const autocolors = window["chartjs-plugin-autocolors"];
 Chart.register(autocolors);
 redraw();
 let addBtn = document.getElementById("add-btn");
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  let formStruct = `<div class="pb-1 pt-2"><input type="text" class="symbol-input form-control" placeholder="symbol"/></div>`;
+  let formStruct = `<div class="row pb-1 pt-2"><div class="col-4"><input type="text" class="symbol-input form-control" placeholder="symbol"/></div></div>`;
   let compareDiv = document.getElementById("compare-div");
   compareDiv.insertAdjacentHTML("beforeend", formStruct);
 });
@@ -141,7 +171,7 @@ let subBtn = document.getElementById("sub-btn");
 subBtn.addEventListener("click", (e) => {
   e.preventDefault();
   let compareDiv = document.getElementById("compare-div");
-  if (compareDiv.children.length > 2) {
+  if (compareDiv.children.length > 1) {
     compareDiv.removeChild(compareDiv.lastChild);
   }
 });
